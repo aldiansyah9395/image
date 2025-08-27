216c417311ce8a0bde06d749d7341eba23b1edf0
aldiansyah9395 <aldiansyah93@outlook.com>
2025-08-27 17:31:00 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 // Konfigurasi API
-const API_URL = 'https://thinks-folding-keyboard-classifieds.trycloudflare.com'; // Ganti menggunakan http://127.0.0.1:8000 jika backend dijalankan di lokal pc
+const API_URL = 'https://locked-classical-spears-annual.trycloudflare.com'; // Ganti menggunakan http://127.0.0.1:8000 jika backend dijalankan di lokal pc
 const API_TIMEOUT = 300000; // 5 menit timeout untuk file besar
 const MAX_FILE_SIZE = 15 * 1024 * 1024; // 5MB batas ukuran file untuk mempercepat upload
 const POLLING_INTERVAL_SMALL = 1000; // Polling interval untuk jumlah gambar kecil (1 detik)
@@ -1395,4 +1395,5 @@ document.addEventListener('DOMContentLoaded', function() {
   });
   
 });
+
 
